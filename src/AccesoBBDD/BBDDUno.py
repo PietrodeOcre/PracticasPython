e08e9f4315755463e072fd8da7340c4cb0c158f0
PietrodeOcre <pietrodeocre@gmail.com>
2019-06-17 21:37:58 +0200

    cambios1 17-06-19

--- src/AccesoBBDD/BBDDUno.py
+++ src/AccesoBBDD/BBDDUno.py
@@ -10,18 +10,23 @@ if __name__ == '__main__':
     
     ############### CONFIGURAR ESTO ###################
     # Abre conexion con la base de datos
-    db = pymysql.connect("localhost","root","Mjcg1610pc1.2","python_pruebas")
+    #db = pymysql.connect("localhost","root","Mjcg1610pc1.2","python_pruebas")
+    #single-priva18.privatednsorg.com:2083
+    #192.168.1.89 Desde Casa
+    #
+    db = pymysql.connect("192.168.1.89","pietro","1234","prueba1")
     ##################################################
 
     # prepare a cursor object using cursor() method
     cursor = db.cursor()
 
     # ejecuta el SQL query usando el metodo execute().
-    #cursor.execute("SELECT VERSION()")
+    cursor.execute("SELECT VERSION()")
     #cursor.execute("CREATE TABLE PRODUCTOS (NOMBRE_ARTICULO VARCHAR(50),PRECIO INTEGER,SECCION VARCHAR(20))")
     #cursor.execute("INSERT INTO PRODUCTOS VALUES('balon',15,'deportes')")
     
     #cursor.commit() solo si se usa sqlite
+    '''
     variosProductos=[
         ("camiseta", 10, "deportes"),
         ("jarron", 10, "ceramica"),
@@ -29,14 +34,29 @@ if __name__ == '__main__':
         ]
     
     cursor.executemany("INSERT INTO PRODUCTOS VALUES(?,?,?)",variosProductos)
-    
+    '''
     # procesa una unica linea usando el metodo fetchone().
     #data = cursor.fetchone()
     #print ("Database version : {0}".format(data))
     
-    data = cursor.execute("SELECT * FROM PRODUCTOS")
-    print ("Primera tupla: ", data)
+    #data = cursor.execute("SELECT * FROM PRODUCTOS")
+    #print ("Primera tupla: ", data)
     
+    
+    query = "SELECT * FROM prueba"
+    
+    cursor.execute(query)
+    
+    datos = cursor.fetchall()
+    
+    for row in datos:
+        id = row[0]
+        nombre = row[1]
+        apellidos = row[2]
+        edad = row[3]
+        print("id = {0}, nombre = {1}, apellidos = {2}, edad = {3}".format(id,nombre,apellidos,edad))
+        
+
 
     # desconecta del servidor
     db.close()
